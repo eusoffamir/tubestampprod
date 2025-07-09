9124bf1850b2671976ff5e107dcbbff8ba46b6d2
eusoffamir <eusoffamir@gmail.com>
2025-07-10 00:20:24 +0800

    Domain connected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './App.css';
 import LandingPage from './unAuth/LandingPage';
 
+import './firebase';
+
+// Redirect users from Firebase default domains to the custom domain
+const defaultHosts = [
+  "tubestampprod-3ff40.web.app",
+  "tubestampprod-3ff40.firebaseapp.com"
+];
+if (defaultHosts.includes(window.location.hostname)) {
+  window.location.href = "https://stamptube.online" + window.location.pathname + window.location.search + window.location.hash;
+}
+
 function App() {
   return (
     <div className="App">
